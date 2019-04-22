67a71a342d56073bab72373330655e78fe58056c
Nadia Lizcano and Sarah Angel <sarah@>
2019-04-22 16:47:49 -0700

    create MSTest configuration model for set ups

---
+++ ProjectName.Tests/ModelTests/ClassNameTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectName;
+
+namespace ProjectName.Tests
+{
+  [TestClass]
+  public class LeapYearTest
+  {
+    // Test methods go here
+  [TestMethod]
+  public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+  {
+    // any necessary logic to prep for test; instantiating new classes, etc.
+    Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
+  }
+
+  }
+}
